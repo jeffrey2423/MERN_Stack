b556a17692f4729c190a1ba4b7119e558e25fede
Jeffrey Rios <jeffrey.rios@correounivalle.edu.co>
2019-10-13 21:12:32 -0500

    configuracion servidor iniciado(rutas API)

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+//SETTINGS
+app.set('port', process.env.PORT ||  4000);
+
+
+//MIDDLEWARES
+//se le dice al servidor que debe enterder json
+//enviar en este caso o entender
+app.use(cors());
+app.use(express.json());
+
+//ROUTES
+//especificamos las urls que la aplicacion de react 
+//podra acceder
+app.get('/api/users', (req, res) => res.send('Users routes'));
+app.get('/api/notes', (req, res) => res.send('Notes routes'));
 
 module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
+
+
 const app = require('./app');
+require('./database');
 
 async function main() {
     //app.listen es un metodo asincrono
     //eso quiere decir que tomara algo de tiempo
     //ejecutarse, y una vez se ejecute se ejecutara el
     //console.log
-    await app.listen(4000);
-    console.log('server on port 4000');
+    await app.listen(app.get('port'));
+    console.log(`Server on port ${app.get('port')}`);
 }
 main();
 
