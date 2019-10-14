487aa0070cf8a90a5d071addef752a86054666ef
Jeffrey Rios <jeffrey.rios@correounivalle.edu.co>
2019-10-13 22:22:58 -0500

    configuracion rutas por HTTPS

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ app.set('port', process.env.PORT ||  4000);
 
 
 //MIDDLEWARES
-//se le dice al servidor que debe enterder json
+//se le dice al servidor que debe entender json
 //enviar en este caso o entender
 app.use(cors());
 app.use(express.json());
@@ -15,7 +15,7 @@ app.use(express.json());
 //ROUTES
 //especificamos las urls que la aplicacion de react 
 //podra acceder
-app.get('/api/users', (req, res) => res.send('Users routes'));
-app.get('/api/notes', (req, res) => res.send('Notes routes'));
+app.use('/api/users', require('./routes/users') );
+app.use('/api/notes', require('./routes/notes'));
 
 module.exports = app;
---
+++ backend/src/routes/users.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const router = Router();
+
+router.route('/')
+//obtener datos
+    .get((req, res) => res.send('Users routes'))
+    //guardar 
+    .post()
+
+
+router.route('/:id')
+    //actualizar un dato
+    .put()
+    //borrar un dato
+    .delete()
+    //actualizar un un dato en especifico
+    .patch()
+
+module.exports = router;
